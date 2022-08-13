c078ffd0876800817baab43d0969239968dd2945
Better_OIer <2155173747@qq.com>
2022-08-13 13:03:06 +0800

    Site updated: 2022-08-13 13:03:04

--- js/DigitalRain.js
+++ js/DigitalRain.js
@@ -1,1 +1,59 @@
-(function(){const scripts=document.getElementsByTagName("script"),script=scripts[scripts.length-1],fontSize=script.getAttribute("size")||16,fontName=script.getAttribute("font")||"monospace";const canvas=document.createElement("canvas"),context=canvas.getContext("2d");document.body.appendChild(canvas);canvas.style.cssText="position: fixed; top: 0; left: 0; background-color: #111; z-index: -2;";let W=window.innerWidth,H=window.innerHeight;canvas.width=W;canvas.height=H;let colunms=Math.floor(W/fontSize),drops=[],str="QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()1234567890qwertyuiopasdfghjklzxcvbnm";for(let i=0;i<colunms;i++){drops.push(Math.ceil(canvas.height/fontSize)+1)}setInterval((function(){context.fillStyle="rgba(0,0,0,0.05)";context.fillRect(0,0,W,H);context.font=`700 ${fontSize}px ${fontName}`;context.fillStyle="#00cc33";for(let i=0;i<colunms;i++){const index=Math.floor(Math.random()*str.length),x=i*fontSize,y=drops[i]*fontSize;context.fillText(str[index],x,y);if(y>=canvas.height&&Math.random()>.99){drops[i]=0}drops[i]++}}),120);window.addEventListener("resize",(function(){W=window.innerWidth;H=window.innerHeight;canvas.width=W;canvas.height=H;colunms=Math.floor(W/fontSize);drops=[];for(let i=0;i<colunms;i++){drops.push(Math.ceil(canvas.height/fontSize)+1)}}))})();
+(function () {
+    const scripts = document.getElementsByTagName("script"),
+        script = scripts[scripts.length - 1],
+        fontSize = script.getAttribute("size") || 12,
+        fontName = script.getAttribute("font") || "monospace";
+    const canvas = document.createElement("canvas"),
+        context = canvas.getContext("2d");
+    document.body.appendChild(canvas);
+    canvas.style.cssText = "position: fixed; top: 0; left: 0; background-color: #111; z-index: -2;";
+    let W = window.innerWidth,
+        H = window.innerHeight;
+    canvas.width = W;
+    canvas.height = H;
+    let colunms = Math.floor(W / fontSize),
+        drops = [],
+        str = "QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()1234567890qwertyuiopasdfghjklzxcvbnm";
+    for (let i = 0; i < colunms; i++) {
+        drops.push(Math.ceil(canvas.height / fontSize) + 1)
+    }
+    function draw(){
+        context.fillStyle = "rgba(238,238,238,.08)";//�ڸǲ�
+        context.fillRect(0,0,W,H);
+        //������������ʽ
+        context.font = "600 "+fontSize+"px  Georgia";
+        //������������ɫ
+        context.fillStyle = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"][parseInt(Math.random() * 10)];//randColor();����rgb,hsl, ��׼ɫ��ʮ��������ɫ
+        //д�뻭����
+        for(var i=0;i<colunms;i++){
+            var index = Math.floor(Math.random() * str.length);
+            var x = i*fontSize;
+            var y = drops[i] *fontSize;
+            context.fillText(str[index],x,y);
+            //���Ҫ�ı�ʱ�䣬�϶����Ǹı�ÿ���������
+            if(y >= canvas.height && Math.random() > 0.99){
+                drops[i] = 0;
+            }
+            drops[i]++;
+        }
+    };
+    function randColor(){//�����ɫ
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        return "rgb("+r+","+g+","+b+")";
+    }
+    draw();
+    setInterval(draw, 35);
+    window.addEventListener("resize", (function () {
+        W = window.innerWidth;
+        H = window.innerHeight;
+        canvas.width = W;
+        canvas.height = H;
+        colunms = Math.floor(W / fontSize);
+        drops = [];
+        for (let i = 0; i < colunms; i++) {
+            drops.push(Math.ceil(canvas.height / fontSize) + 1)
+        }
+    }))
+})();
